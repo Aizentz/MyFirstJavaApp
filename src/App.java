542cd5f5620af283b2eaf2d54c388634f6b6b0ae
Gabriel Abner <gabrielabner134@gmail.com>
2025-04-18 11:35:50 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) {
+        var scanner = new java.util.Scanner(System.in);
+
+        System.out.println("Conversor de fahrenheitº para Celcius");
+
+        System.out.print("Digite quantos graus Fº: ");
+        double num1 = Double.parseDouble(scanner.nextLine());
+
+        double f = (num1 - 32)/ 9 * 5;
+        
+        System.out.println("parece que a temperatura é de "+ f +" Cº");
+
+    }
+}
